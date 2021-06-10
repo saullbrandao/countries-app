d1077125474f38e3a0f26b4a76d2b958396ba1bc
Saull Brandão <saull@outlook.com>
2021-06-10 10:31:42 -0300

    feat: change the card component to use the data passed as props from the api

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -1,14 +1,17 @@
-export const CountryCard = () => {
+import Image from 'next/image'
+
+export const CountryCard = ({ countryData }) => {
   return (
     <div className="flex flex-col bg-white rounded-md self-center">
-      <div className='h-40 w-72 bg-dark-background rounded-md'>
+      <div className='h-40 w-60 bg-dark-background rounded-md'>
+        <Image src={countryData.flag} width={300} height={200} />
       </div>
       <div className="pl-6 pt-6 mb-12">
 
-        <h2 className='font-bold text-lg mb-2' >Germany</h2>
-        <p><strong className='font-semibold'>Population:</strong> 87,770,900</p>
-        <p><strong className='font-semibold'>Region:</strong> Europe</p>
-        <p><strong className='font-semibold'>Capital:</strong> Berlin</p>
+        <h2 className='font-bold text-lg mb-2' >{countryData?.name}</h2>
+        <p><strong className='font-semibold'>Population:</strong> {countryData?.population}</p>
+        <p><strong className='font-semibold'>Region:</strong> {countryData?.region}</p>
+        <p><strong className='font-semibold'>Capital:</strong> {countryData?.capital}</p>
       </div>
     </div>
   )
