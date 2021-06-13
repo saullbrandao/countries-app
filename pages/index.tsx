4a4c1f633982e61bf22fce5773e1412dccd2155a
Saull Brandão <saull@outlook.com>
2021-06-13 09:15:44 -0300

    feat: change the search icon color based on theme selected

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import ReactList from 'react-list'
 import { SearchIcon } from "@heroicons/react/outline";
+import { useDarkMode } from "../contexts/DarkModeContext";
 
 const getCountries = async () => {
   const response = await axios.get('https://restcountries.eu/rest/v2/all')
@@ -12,6 +13,7 @@ const getCountries = async () => {
 }
 
 export default function Home() {
+  const { darkMode } = useDarkMode()
   const { data, isError, isFetched, isLoading, refetch } = useQuery('countries', getCountries, { refetchOnWindowFocus: false })
 
   return (
@@ -20,7 +22,7 @@ export default function Home() {
       <div className="flex flex-col gap-8">
         <div className="flex items-center mx-4 mb-4 h-14 shadow-md rounded-md p-4 dark:bg-dark-elements focus-within:ring-1 focus-within:ring-light-text dark:focus-within:ring-white">
           <div className="h-full flex justify-center items-center">
-            <SearchIcon stroke='white' className='h-6' />
+            <SearchIcon stroke={`${darkMode ? 'white' : 'black'}`} strokeOpacity='50' color='white' className='h-6' />
           </div>
           <input
             className='ml-4 pl-4 h-full text-light-input dark:text-white dark:bg-dark-elements outline-none'
