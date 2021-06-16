ff068f823aaef952d021c2737714f085cf12cc4b
Saull Brandão <saull@outlook.com>
2021-06-16 10:07:17 -0300

    feat: remove spaces from the url

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link'
 
 export const CountryCard = ({ countryData }) => {
   return countryData && (
-    <Link href={`/countries/${countryData.name}`} passHref={true}>
-      <div className='w-full flex mb-6 justify-center'>
-        <div className="flex flex-col bg-white dark:bg-dark-elements dark:text-white rounded-md w-60">
+    <div className='w-full flex mb-6 justify-center'>
+      <Link href={`/countries/${countryData.name.split(' ').join('-')}`}>
+        <div className="flex flex-col bg-white dark:bg-dark-elements dark:text-white rounded-md w-60 cursor-pointer">
           <Image className='rounded-md' src={countryData.flag} width={300} height={200} objectFit='cover' />
           <div className="pl-6 pt-6 mb-12">
             <h2 className='font-bold text-lg mb-2' >{countryData.name}</h2>
@@ -15,7 +15,7 @@ export const CountryCard = ({ countryData }) => {
             <p><strong className='font-semibold'>Capital:</strong> {countryData.capital}</p>
           </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
