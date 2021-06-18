bd24e094109d8e4e0618b0749e667d6e2983a200
Saull Brandão <saull@outlook.com>
2021-06-18 17:34:37 -0300

    refactor: change from grid to flex

--- components/CountriesList.tsx
+++ components/CountriesList.tsx
@@ -2,7 +2,7 @@ import { CountryCard } from "./CountryCard"
 
 export const CountriesList = ({ countries }) => {
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  xl:grid-cols-4 2xl:grid-cols-5 justify-items-center gap-4' >
+    <div className='flex flex-wrap justify-center sm:justify-between gap-2' >
       {countries.map(country => <CountryCard key={country.name} countryData={country} />)}
     </div>
   )
