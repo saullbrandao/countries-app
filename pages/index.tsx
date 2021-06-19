06aff9981d90fac89406db979b0626e99da01cd8
Saull Brandão <saull@outlook.com>
2021-06-19 10:43:34 -0300

    feat: implement country search

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { RegionFilter } from "../components/RegionFilter";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import axios from "axios";
-import { SearchField } from '../components/SearchField';
+import { SearchInput } from '../components/SearchInput';
 
 const getCountries = async () => {
   const response = await axios.get('https://restcountries.eu/rest/v2/all?fields=name;region;capital;population;flag')
@@ -18,20 +18,30 @@ export default function Home() {
   const [filteredCountries, setFilteredCountries] = useState([])
   const [displayedCountries, setDisplayedCountries] = useState([])
 
+  useEffect(() => {
+    const handleCountrySearch = () => {
+      const searchResult = filteredCountries.filter(country => country.name.toLowerCase().includes(searchTerm.toLowerCase()))
+      setDisplayedCountries(searchResult)
+    }
+
+    handleCountrySearch()
+  }, [searchTerm, filteredCountries])
 
   useEffect(() => {
     const handleRegionSelection = () => {
       const filteredCountries = filter ? countries.filter(country => country.region.toLowerCase() === filter.toLowerCase()) : countries
-      setDisplayedCountries(filteredCountries)
+      setFilteredCountries(filteredCountries)
     }
+
     countries && handleRegionSelection()
   }, [countries, filter])
+
 
   return (
     <div className='container mx-auto min-h-screen'>
       <div className="flex flex-col gap-8 ">
         <div className="flex flex-col mx-4 md:mx-0 md:flex-row md:justify-between">
-          <SearchField handleSearchTerm={setSearchTerm} />
+          <SearchInput handleSearchTerm={setSearchTerm} />
           <RegionFilter filter={filter} handleFilter={(region: string) => setFilter(region)} />
         </div>
         {isError && <h1>Error</h1>}
