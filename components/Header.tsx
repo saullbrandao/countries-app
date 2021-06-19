01bac7636b2f2b8811c525918d511f1dedc36913
Saull Brandão <saull@outlook.com>
2021-06-19 09:21:45 -0300

    fix: center header elements relative to the page

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,11 @@ import { DarkModeToggle } from "./DarkModeToggle"
 
 export const Header = () => {
   return (
-    <header className='flex justify-between px-4 py-8 mb-8 w-full bg-white dark:bg-dark-elements shadow-md text-lg text-light-text'>
-      <span className='font-bold dark:text-white whitespace-nowrap'>Where in the world?</span>
-      <DarkModeToggle />
+    <header className='bg-white dark:bg-dark-elements shadow-md text-lg text-light-text'>
+      <div className='container mx-auto flex justify-between px-2 md:px-0 py-8 mb-8 w-full '>
+        <span className='font-bold dark:text-white whitespace-nowrap'>Where in the world?</span>
+        <DarkModeToggle />
+      </div>
     </header>
   )
 }
