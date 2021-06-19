41132298e93693e324e2f6af8b6c8c96815341a1
Saull Brandão <saull@outlook.com>
2021-06-19 09:24:32 -0300

    feat: make the country page responsive

--- pages/countries/[slug].tsx
+++ pages/countries/[slug].tsx
@@ -37,25 +37,34 @@ export default function CountryInfo() {
   useEffect(() => {
     slug && refetch()
   }, [slug, refetch])
-
+  // finish responsive design
   return (
-    <div className="min-h-screen mx-6 flex flex-col">
+    <div className="min-h-screen flex flex-col mx-4 md:container md:mx-auto gap-4 md:gap-12">
       <Link href='/'><a className='flex w-28 h-8 items-center justify-center gap-2 mb-8 bg-white dark:bg-dark-elements dark:text-white rounded-md shadow-lg' ><ArrowLeftIcon className='h-6' /> Back</a></Link>
       {isError && <h1>Error</h1>}
       {isFetched && !isError ?
-        <div className="flex flex-col gap-6  dark:text-white rounded-md">
-          <Image className='rounded-md' src={countryData.flag} width={600} height={400} objectFit='cover' alt={`${countryData.name}'s flag`} />
-          <div className='flex flex-col gap-1' >
-            <h2 className='font-bold text-2xl mb-2' >{countryData.name}</h2>
-            <p><strong className='font-semibold'>Native Name:</strong> {countryData.nativeName}</p>
-            <p><strong className='font-semibold'>Population:</strong> {countryData.population}</p>
-            <p><strong className='font-semibold'>Region:</strong> {countryData.region}</p>
-            <p><strong className='font-semibold'>Sub Region:</strong> {countryData.subregion}</p>
-            <p className='mb-8'><strong className='font-semibold'>Capital:</strong> {countryData.capital}</p>
-            <p><strong className='font-semibold'>Top Level Domain:</strong> {countryData.topLevelDomain[0]}</p>
-            <p><strong className='font-semibold'>Currencies:</strong> {countryData.currencies.map(currency => `${currency} `)}</p>
-            <p className='mb-8'><strong className=' font-semibold'>Languages:</strong> {countryData.languages.map(language => `${language} `)}</p>
+        <div className="flex flex-col lg:flex-row lg:justify-evenly gap-12 mb-8 dark:text-white rounded-md">
+          <Image className='rounded-md' src={countryData.flag} width={720} height={480} objectFit='cover' alt={`${countryData.name}'s flag`} />
+          <div className='flex flex-col gap-4 lg:grid grid-cols-2' >
+            <h2 className='font-bold text-2xl mb-2 col-span-full ' >{countryData.name}</h2>
             <div className="">
+
+              <p><strong className='font-semibold'>Native Name:</strong> {countryData.nativeName}</p>
+              <p><strong className='font-semibold'>Population:</strong> {new Intl.NumberFormat('pt-BR').format(countryData.population)}</p>
+              <p><strong className='font-semibold'>Region:</strong> {countryData.region}</p>
+              <p><strong className='font-semibold'>Sub Region:</strong> {countryData.subregion}</p>
+              <p className='mb-8'><strong className='font-semibold'>Capital:</strong> {countryData.capital}</p>
+            </div>
+
+            <div className="">
+
+              <p><strong className='font-semibold'>Top Level Domain:</strong> {countryData.topLevelDomain[0]}</p>
+              <p><strong className='font-semibold'>Currencies:</strong> {countryData.currencies.join(', ')}</p>
+              <p className='mb-8'><strong className=' font-semibold'>Languages:</strong> {countryData.languages.join(', ')}</p>
+            </div>
+
+
+            <div className="col-span-full">
               <h2 className='font-semibold text-lg mb-2' >Border Countries:</h2>
               <div className='grid grid-cols-3 gap-2'>
                 {countryData.borders ? countryData.borders.map(countryName => <Link key={countryName} href={`/countries/${countryName.split(' ').join('-')}`} >
