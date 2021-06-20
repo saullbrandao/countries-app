ffe2aeeebc3c791fbdc37eba8c7d82ec7c0514e6
Saull Brandão <saull@outlook.com>
2021-06-20 10:23:26 -0300

    feat: add scale effect when hovering the card

--- components/CountryCard.tsx
+++ components/CountryCard.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 export const CountryCard = ({ countryData }) => {
   return countryData && (
     <Link href={`/countries/${countryData.name.split(' ').join('-')}`} passHref>
-      <div className="flex flex-col bg-white dark:bg-dark-elements dark:text-white rounded-md w-60 cursor-pointer">
+      <li className="flex flex-col bg-white dark:bg-dark-elements dark:text-white rounded-md w-60 cursor-pointer hover:transform hover:scale-105 hover:shadow-lg hover:border border-black border-opacity-20">
         <Image className='rounded-md' src={countryData.flag} width={300} height={200} objectFit='cover' alt={`${countryData.name}'s flag`} />
         <div className="pl-6 pt-6 mb-12">
           <h2 className='font-bold text-lg mb-2' >{countryData.name}</h2>
@@ -13,7 +13,7 @@ export const CountryCard = ({ countryData }) => {
           <p><strong className='font-semibold'>Region:</strong> {countryData.region}</p>
           <p><strong className='font-semibold'>Capital:</strong> {countryData.capital}</p>
         </div>
-      </div>
+      </li>
     </Link>
   )
 }
